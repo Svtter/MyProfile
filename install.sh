cd6e8f869abd34aacb1b0b655fe4b385f5d0adb2
svtter <svtter@qq.com>
2015-03-30 21:38:07 +0800

    change .profile to profile

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln profile $HOME/.profile
-echo "source $HOME/.profile" >> $HOME/.bashrc
-echo "source $HOME/.profile" >> $HOME/.zshrc
+echo "source $HOME/profile" >> $HOME/.bashrc
+echo "source $HOME/profile" >> $HOME/.zshrc
 
 echo Done
