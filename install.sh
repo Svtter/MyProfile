6681abfa172c8fe1b83c6c8e390c43a413c6cb5f
svtter <svtter@qq.com>
2015-04-26 22:55:56 +0800

    add python-startup-completation

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 ln profile $HOME/profile
 echo "source $HOME/profile" >> $HOME/.bashrc
 echo "source $HOME/profile" >> $HOME/.zshrc
+mkdir -p $HOME/python/
+echo "import readline, rlcompleter; readline.parse_and_bind("tab: complete")" >> $HOME/python/.pythonstartup.py
 
 echo Done
